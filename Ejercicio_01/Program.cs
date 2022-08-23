59b17d90f23982236e2a01df6a4659fd3887f430
Gonzzaa1 <98622541+Gonzzaa1@users.noreply.github.com>
2022-08-22 23:37:50 -0300

    Ejercicios realizados

--- Ejercicio_01/Program.cs
+++ Ejercicio_01/Program.cs
@@ -6,7 +6,37 @@ namespace Ejercicio_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeros = new int[5];
+
+            int numeroMin = 0;
+            int numeroMax = 0;
+            int acumulador = 0;
+            int promedio;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Ingrese un numero: ");
+
+                numeros[i] = int.Parse(Console.ReadLine());
+                acumulador += numeros[i];
+
+                if (i == 0)
+                {
+                    numeroMax = numeros[i];
+                    numeroMin = numeros[i];
+                }
+                else if (numeroMax > numeros[i])
+                {
+                    numeroMax = numeros[i];
+                }
+                else if(i == 0 || numeroMin < numeros[i])
+                {
+                    numeroMin = numeros[i]; 
+                }
+
+            }
+            promedio = acumulador / 5;
+            Console.WriteLine($"El numero maximo es {numeroMax}, El numero minimo es {numeroMin} y el promedio es {promedio}");
         }
     }
 }
